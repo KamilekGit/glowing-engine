c18db810314f51c4fd55c266fa63392ed1699089
KamilekGit <136954316+KamilekGit@users.noreply.github.com>
2023-12-14 16:02:20 +0300

    Add files via upload

---
+++ Clock/Controllers/CarsController.cs
@@ -0,0 +1,30 @@
+﻿using Clock.Data.interfaces;
+using Clock.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Clock.Controllers
+{
+    public class CarsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        private readonly IAllCars _allCars;
+       private readonly ICarsCategory _allCategories;
+        public CarsController(IAllCars iallcars, ICarsCategory icarscategory) { 
+        _allCars = iallcars;
+            _allCategories = icarscategory;
+            
+        }
+        public ViewResult List()
+        {
+            ViewBag.Title = "Страница с автомобилями";
+            CarsListViewModel obj = new CarsListViewModel();
+            obj.allCars = _allCars.GetFavCars;
+            obj.currCategory = "автомобили";
+
+            return View(obj);
+        }
+    }
+}
---
+++ Clock/ViewModels/CarsListViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Clock.Data.Models;
+
+namespace Clock.ViewModels
+{
+    public class CarsListViewModel
+    {
+        public IEnumerable<Car> allCars { get; set; }
+        public string currCategory {  get; set; }
+    }
+}
